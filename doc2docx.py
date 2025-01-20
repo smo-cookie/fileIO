ec649c02ce4f6a1d1789f62b27f7cfc35a6c284f
sultanofdisco <87179738+sultanofdisco@users.noreply.github.com>
2025-01-20 12:18:40 +0000

    Update doc2docx.py
    모듈화시킴

--- doc2docx.py
+++ doc2docx.py
@@ -2,15 +2,39 @@ import glob
 import re
 import os
 import win32com.client
-import sys
 
-for f in glob.glob('*.doc'):
-    p = os.getcwd() + os.sep + f
-    print(f'Converting {f} to docx...')
+def convert_doc_to_docx(directory='.'):
+    """
+    Converts all .doc files in the specified directory to .docx format.
+
+    Parameters:
+    directory (str): The directory to search for .doc files. Defaults to the current directory.
+
+    Returns:
+    None
+    """
     word = win32com.client.Dispatch('Word.Application')
-    doc = word.Documents.Open(p)
+    
+    try:
+        doc_files = glob.glob(os.path.join(directory, '*.doc'))
+        if not doc_files:
+            print('No .doc files found.')
+            return
 
-    doc.SaveAs(re.sub(r'\.\w+$', '.docx', os.path.abspath(p)), 16)
-    doc.Close()
-else:
-    print('Done.')
+        for file in doc_files:
+            full_path = os.path.abspath(file)
+            print(f'Converting {file} to docx...')
+            
+            doc = word.Documents.Open(full_path)
+            new_path = re.sub(r'\.\w+$', '.docx', full_path)
+            doc.SaveAs(new_path, 16)  # 16 is the format code for .docx
+            doc.Close()
+            
+        print('Conversion completed.')
+    finally:
+        word.Quit()
+
+if __name__ == "__main__":
+    # Replace '.' with the desired directory if needed
+    convert_doc_to_docx('.')
+
